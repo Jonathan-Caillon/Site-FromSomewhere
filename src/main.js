a09944226505d9046d32213dd13f894189db6719
Jonathan-Caillon <j.caillon@codeur.online>
2021-12-15 14:11:08 +0100

    fix: view error

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
 createApp(App).use(router).mount("#app");
-
-app.set("view engine", "vue");
