918b5850a98926a7fa582221ca3667d66877844e
Jonathan-Caillon <j.caillon@codeur.online>
2021-12-15 11:37:03 +0100

    models

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+createApp(App).use(router).mount("#app");
 
-createApp(App).use(router).mount('#app')
+app.set("view engine", "vue");
