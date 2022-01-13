1255faddb3c47661ac1790151cc2f90f1722bfa9
ahamm-prequal2021 <a.hamm@codeur.online>
2022-01-13 17:05:18 +0100

    responsive landing page done (except modal)

--- client/src/assets/js/app.js
+++ client/src/assets/js/app.js
@@ -36,7 +36,23 @@ window.onload = function(){
         });
         e.addEventListener("mouseenter", function(){
             if(!e.classList.contains("active")){
-                
+                if(e.classList.contains("game1")){
+                    document.querySelector(".fs-games").style.transform = "translate(-7%)"
+                }
+                else if(e.classList.contains("game2")){
+                    document.querySelector(".fs-games").style.transform = "translate(-8.5%)"
+                }
+                else if(e.classList.contains("game4")){
+                    document.querySelector(".fs-games").style.transform = "translate(-12.5%)"
+                }
+                else if(e.classList.contains("game5")){
+                    document.querySelector(".fs-games").style.transform = "translate(-15%)"
+                }
+                document.querySelectorAll(".fs-games__item").forEach(function(el){
+                    el.classList.remove("hovered");
+                    document.querySelector(".fs-games").style.transform = "translate(-10%)"
+                });
+                e.classList.add("hovered");
             }
         })
         
