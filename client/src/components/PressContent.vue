cea9c8479457faff3d80469cfa0162208d5a7a9d
Nico_SicariusFR <n.schmitt@codeur.online>
2022-01-19 16:51:16 +0100

    feat: modals v4 WIP

--- client/src/components/PressContent.vue
+++ client/src/components/PressContent.vue
@@ -2,18 +2,21 @@
 
   <div class="press-container">
 
-    <div class="press-article" v-for="article in articles" :key="article.id" @click="toggleModal">
-      <div class="articleBody" @click="showModal">
-        <PressModal :reveal="reveal" :toggleModal="toggleModal" />
-        <img class="article-image" :src="`/src/assets/img/`+ article.imageName" alt="">
+    <div class="press-article" v-for="article in articles" :key="article.id" @click="toggleModalON">
+      <div class="articleBody">
+        <img class="article-image" :src="require('../assets/img/'+article.imageName)" alt="">
         <div class="article-txt">
-          <div class="article-title">{{ article.title }} </div>
+          <div class="article-title">{{ article.title }} </div><br>
           <div class="article-excerpt">{{ article.excerpt }}</div>
         </div>
       </div>
-
-      <!-- MODAL -->
-
+      <div class="bloc-modal">
+        <div class="overlay" @click="toggleModalOFF"></div>
+        <div class="modal">
+          <button class="btn-modal" @click="toggleModalOFF">exit</button>
+          <div class="modal-body">{{article.body}}</div>
+        </div>
+      </div>
     </div> 
 
   </div>
@@ -23,24 +26,26 @@
 <script>
 
 import axios from 'axios';
-import PressModal from '@/components/PressModal.vue';
 
 const API = '/api/press/';
+
 
 export default {
   name: "PressContent",
   components: {
-    PressModal,
+
   },
   data(){
     return {
       articles: [],
-      reveal: false,
     }
   },
   methods: {
-    toggleModal: function(){
-      this.reveal = !this.reveal
+    toggleModalON: function(){
+      document.querySelector(".bloc-modal").style.display = "flex"
+    },
+    toggleModalOFF: function(){
+      document.querySelector(".bloc-modal").style.display = "none"
     },
   },
   created(){
@@ -51,7 +56,7 @@ export default {
       })
       .catch(error => {
         console.log(error)
-      })
+      });
   }
 }
 
@@ -67,7 +72,7 @@ export default {
   margin-left: 25vw;
 }
 
-.article-body{
+.articleBody{
   display: flex;
   margin-top: 2%;
   cursor: pointer;
@@ -86,5 +91,37 @@ export default {
 
 /* MODAL */
 
+.bloc-modal{
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: none;
+  justify-content: center;
+  align-items: center;
+}
+
+.overlay{
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.modal{
+  background: #f1f1f1;
+  padding: 50px;
+  position: fixed;
+  top: 30%;
+}
+
+.btn-modal{
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
 
 </style>
