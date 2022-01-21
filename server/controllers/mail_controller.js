2386b0d31d5f5c8a5b8104f3ae960f5dc7ed680d
Jonathan-Caillon <j.caillon@codeur.online>
2022-01-21 17:52:33 +0100

    post mail + backoffice

---
+++ server/controllers/mail_controller.js
@@ -0,0 +1,43 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
+});
+
+const createData = (req, res) => {
+  let from = req.body.from;
+  let subject = req.body.subject;
+  let content = req.body.content;
+
+  const mail = {
+    from: from,
+    to: process.env.EMAIL,
+    subject: subject,
+    html: '<p style="font-size:25px">' + content + "</p><br>" + from,
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      console.log(err);
+      // res.status(500).send("Something went wrong."); éviter le status 500, peut faire cracher la page
+      res.status(200).json({
+        msg: "Une erreur est survenue, merci de rééssayer plus tard.",
+      });
+    } else {
+      // console.log("hilo");
+      res.status(200).json({
+        msg: "OK",
+      });
+    }
+  });
+};
+
+module.exports = {
+  createData,
+};
