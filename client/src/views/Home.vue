869ca49352b907639b03addae6cd8fd71d12f2cf
ahamm-prequal2021 <a.hamm@codeur.online>
2022-01-24 16:25:41 +0100

    fix js landing page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -68,7 +68,6 @@
 <script>
 // @ is an alias to /src
 import axios from 'axios';
-import "../assets/js/app.js";
 export default {
   name: "Home",
   data(){
@@ -86,6 +85,57 @@ export default {
     .catch((error) => {
       console.log(error);
     });
+    setTimeout(function(){
+        document.querySelector('.game2').classList.add('hovered');
+    
+
+        document.querySelectorAll(".fs-games__item").forEach(function(e){
+            let enlarge = false;
+            e.querySelector('img').addEventListener("click", function(){
+                if(enlarge == false){
+                    e.classList.toggle("active");
+                    document.querySelectorAll(".fs-games__item").forEach(function(element){
+                        if(element.classList.contains("active")){
+                            element.classList.remove("hovered");
+                            setTimeout(function(){
+                                element.querySelector(".fs-games__item__close").style.display = "block";
+                            }, 200);
+                            element.classList.add('large');
+                            enlarge = true;
+                        }else{
+                            element.classList.add("mobile");
+                            element.style.opacity = "0";
+                        }
+                        document.querySelector(".fs-games").classList.add('enlarged');
+                    });
+                }
+            })
+            e.querySelector(".fs-games__item__close").addEventListener('click', function(){
+                document.querySelectorAll(".fs-games__item").forEach(function(element){
+                    if(element.classList.contains("active")){
+                        document.querySelector(".fs-games").classList.remove("enlarged");
+                        element.querySelector(".fs-games__item__close").style.display = "none";
+                        element.classList.remove("large");
+                        element.classList.remove("active");
+                        element.classList.add("hovered");
+                    }else{
+                        enlarge = false;
+                        element.style.opacity = "1";
+                        element.classList.remove("mobile");
+                    }
+                });
+            });
+            e.addEventListener("mouseenter", function(){
+                if(!e.classList.contains("active")){
+                    document.querySelectorAll(".fs-games__item").forEach(function(el){
+                        el.classList.remove("hovered");
+                    });
+                    e.classList.add("hovered");
+                }
+            })
+            
+        })
+    }, 100);
   }
 };
 </script>
